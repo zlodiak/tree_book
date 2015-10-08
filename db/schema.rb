9376660e96b0febe932441dddee8755e4fc20656
zlodiak <prozaik81-2@yandex.ru>
2015-10-08 12:12:43 +0300

    ancestry migrate ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008084841) do
+ActiveRecord::Schema.define(version: 20151008091101) do
 
   create_table "messages", force: :cascade do |t|
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "ancestry"
   end
+
+  add_index "messages", ["ancestry"], name: "index_messages_on_ancestry"
 
 end
